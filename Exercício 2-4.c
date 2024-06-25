b1e7dbab6bd2ac831a2f9cbf013efb57e46d0ce6
jspedrok <pedro2008nasc@gmail.com>
2024-06-25 14:53:11 -0300

    Add files via upload

---
+++ Exercício 2-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int comprar_itens (){
+ int limite_itens = 3;
+            return limite_itens;
+ }
+    
+int main ()
+{  
+          
+        printf("Exercicio 2:\n");
+printf ("itens comprados: %d\n", comprar_itens());
+ 
+   return 0;
+}
